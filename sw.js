beef6399e2d98b96b86ff10d22e2e3dcf4a631a1
JungSik Jeong <wndtlr1024@gmail.com>
2024-02-19 19:48:59 +0900

    Updates

--- sw.js
+++ sw.js
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "51ee72a32ce7c290a92e9a76ef350d11"
+    "revision": "c9fe60f37d7dc944d4316f0bda03638f"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
